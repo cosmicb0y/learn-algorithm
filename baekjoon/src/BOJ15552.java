636f7bc646b2ece5f4c5d92b9253224c7d7752fa
cosmicb0y <jang.hw73@gmail.com>
2018-09-27 17:00:02 +0900

    Solved Baekjoon 15552

---
+++ baekjoon/src/BOJ15552.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0 ;i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(a + b + "\n");
+        }
+
+        bw.flush();
+    }
+}
