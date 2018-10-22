ee402696e54bc95e3734dadb430b08740def5c39
cosmicb0y <jang.hw73@gmail.com>
2018-10-22 23:35:08 +0900

    Solved Baekjoon 15964

---
+++ baekjoon/src/BOJ15964.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BOJ15964 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        BigInteger a = new BigInteger(input[0]);
+        BigInteger b = new BigInteger(input[1]);
+
+        System.out.println(a.add(b).multiply(a.subtract(b)).toString());
+    }
+}
